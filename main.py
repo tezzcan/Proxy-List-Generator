58e76d39200e63bafa8de6bf741605c08695b17a
Oğuzhan Tezcan <aslanozi55@gmail.com>
2020-05-17 15:53:25 +0300

    Importing JSON
    I've tried the implement json module.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.support.ui import Select
+import json
+
+data = {}
+data['proxies'] = []
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless') # this shit aint doin nothin
 
-driverPath = 'chromedriver.exe' # Specify the chrome driver location. LOL I DID :D
+driverPath = 'chromedriver.exe' # Specify the chrome driver location. ( LOL I DID :D )
 url = 'https://free-proxy-list.net/' # Of course you can change this bruuuh.
 
 driver = webdriver.Chrome(executable_path=driverPath)
@@ -14,11 +18,23 @@ driver.get(url)
 select = Select(driver.find_element_by_xpath('//*[@id="proxylisttable_length"]/label/select'))
 select.select_by_value('80')
 
-for i in range(1,81):
-    with open('proxylist.txt','a',encoding='utf8') as f:
-        ip = driver.find_element_by_xpath(f'//*[@id="proxylisttable"]/tbody/tr[{i}]/td[1]').text
-        port = driver.find_element_by_xpath(f'//*[@id="proxylisttable"]/tbody/tr[{i}]/td[2]').text
-        proxy = "'http'" + ' : ' + f"'http://{ip}:{port}',"
+f = open('proxylist.txt','a',encoding='utf8')
 
-        print(proxy)
-        f.write(proxy + '\n')
+for i in range(1,81):
+    ip = driver.find_element_by_xpath(f'//*[@id="proxylisttable"]/tbody/tr[{i}]/td[1]').text
+    port = driver.find_element_by_xpath(f'//*[@id="proxylisttable"]/tbody/tr[{i}]/td[2]').text
+    #proxy = "'http'" + ' : ' + f"'http://{ip}:{port}',"
+    proxy = f"'http' : 'http://{ip}:{port}',\n"
+
+    data['proxies'].append({
+        'ip' : f'{ip}',
+        'port' : f'{port}'
+    })
+
+    print(proxy)
+    f.write(proxy)
+
+f.close()
+
+with open('proxylist.json','a',encoding='utf8') as p:
+    json.dump(data,p)
